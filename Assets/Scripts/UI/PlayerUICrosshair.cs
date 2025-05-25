aa25b69d4149192d238a3afd972bcd1b1e264c6d
Piotr Cynowski <piotr.peter.cynowski@gmail.com>
2025-05-25 21:02:57 +0200

    add jump/land update position for crosshair ui

--- Assets/Scripts/UI/PlayerUICrosshair.cs
+++ Assets/Scripts/UI/PlayerUICrosshair.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Player;
 
 public class PlayerUICrosshair : MonoBehaviour
 {
@@ -9,27 +10,41 @@ public class PlayerUICrosshair : MonoBehaviour
     [SerializeField] private float moveSwayAmount = 10f;
     [SerializeField] private float mouseSwayAmount = 5f;
 
+    [SerializeField] private float jumpSwayAmount = 0.03f;
+    [SerializeField] private float landBounceAmount = 0.05f;
+    [SerializeField] private float impulseDecayTime = 0.2f;
+    private Vector2 crosshairImpulse = Vector2.zero;
+    private Vector2 crosshairImpulseVelocity = Vector2.zero;
+
     private Vector2 originalPosition;
 
     void Start()
     {
         originalPosition = crosshair.anchoredPosition;
+        PlayerMovement.onJump += OnJumpOrLandCrosshair;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerMovement.onJump -= OnJumpOrLandCrosshair;
     }
 
     public void UpdateCrosshair(Vector2 mouseInput, Vector2 keyboardInput)
     {
-        float horizontalMove = keyboardInput.x;
-        float verticalMove = 0;
+        Vector2 moveOffset = new Vector2(keyboardInput.x, 0f).normalized * moveSwayAmount;
 
-        Vector2 moveOffset = new Vector2(horizontalMove, verticalMove).normalized * moveSwayAmount;
+        Vector2 mouseOffset = new Vector2(-mouseInput.x, mouseInput.y) * mouseSwayAmount;
 
-        float mouseX = mouseInput.x;
-        float mouseY = mouseInput.y;
+        crosshairImpulse = Vector2.SmoothDamp(crosshairImpulse, Vector2.zero, ref crosshairImpulseVelocity, impulseDecayTime);
 
-        Vector2 mouseOffset = new Vector2(-mouseX, mouseY) * mouseSwayAmount;
-        Vector2 totalOffset = moveOffset + mouseOffset;
+        Vector2 totalOffset = moveOffset + mouseOffset + crosshairImpulse;
         Vector2 targetPosition = originalPosition + totalOffset;
 
         crosshair.anchoredPosition = Vector2.SmoothDamp(crosshair.anchoredPosition, targetPosition, ref velocity, smoothTime);
+    }
+
+    public void OnJumpOrLandCrosshair(bool isJump)
+    {
+        crosshairImpulse = new Vector2(0f, isJump ? - jumpSwayAmount * 100f : landBounceAmount * 100f);
     }
 }
