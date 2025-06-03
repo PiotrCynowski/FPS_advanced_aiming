e4f6702b499abc4f1550c45e5914b3c3f955d902
Piotr Cynowski <piotr.peter.cynowski@gmail.com>
2025-06-03 19:03:31 +0200

    Update

--- Assets/Scripts/Weapons/BulletGrenade.cs
+++ Assets/Scripts/Weapons/BulletGrenade.cs
@@ -4,10 +4,19 @@ namespace Weapons
 {
     public class BulletGrenade : Bullet
     {
+        [SerializeField] private float itemThrowForce;
+
+        private Rigidbody rb;
+        private Collider coll;
+
         public override void UpdateBullet()
         {
            
         }
 
+        public void ThrowItem(Vector3 direction, float force)
+        {
+            rb.AddForce(direction * force, ForceMode.Impulse);
+        }
     }
 }
