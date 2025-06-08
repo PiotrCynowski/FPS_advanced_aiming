ade2794e1a047cfe7153498358e7bf800509a204
Piotr Cynowski <piotr.peter.cynowski@gmail.com>
2025-06-08 16:27:37 +0200

    Update

---
+++ Assets/Scripts/ItemObjects/Animated/ICanBeAnimated.cs
@@ -0,0 +1,5 @@
+public interface ICanBeDoorAnimated
+{
+    void SwitchAnimOpenCloseDoor();
+    bool IsDoorOpen();
+}
--- Assets/Scripts/ItemObjects/InteractableObject.cs
+++ Assets/Scripts/ItemObjects/InteractableObject.cs
@@ -1,7 +1,22 @@
-public class InteractableObject : InteractableObj, ICanBeInteracted
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableObject : InteractableObj, ICanBeInteracted, ICanBeDoorAnimated
 {
+    [SerializeField] UnityEvent onInteraction;
+
+    public bool IsDoorOpen()
+    {
+        throw new System.NotImplementedException();
+    }
+
     public void OnInteracted()
     {
-       
+        onInteraction?.Invoke();
+    }
+
+    public void SwitchAnimOpenCloseDoor()
+    {
+        throw new System.NotImplementedException();
     }
 }
